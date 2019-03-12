875fe867615b7499722b2befe198c1e82a030664
Romain <romain.chipon77@gmail.com>
2019-03-12 11:15:08 +0100

    debut exo 2

--- src/testTechnique/RaceResults.java
+++ src/testTechnique/RaceResults.java
@@ -1,5 +1,15 @@
 package testTechnique;
 
 public class RaceResults {
-
+	public TimeDuration time;
+	public String RFID;
+	
+	RaceResults(TimeDuration x, String y){
+		time=x;
+		RFID=y;
+	}
+	
+	void onNewResult(String tagNumber, TimeDuration resultTime) {
+		
+	}
 }
